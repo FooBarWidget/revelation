e01e1aa2ab18c70ddbbb85aa41b4d98f214b6c3a
Hongli Lai (Phusion) <hongli@phusion.nl>
2009-04-08 14:06:55 +0200

    Fix some build system issues.

--- autogen.sh
+++ autogen.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-srcdir=`basename $0`
+srcdir=`dirname "$0"`
 [ -z "$srcdir" ] && srcdir=.
 
-PKG_NAME=mypackage
+PKG_NAME=revelation
 REQUIRED_AUTOMAKE_VERSION=1.7
 
-if [ ! -f "$srcdir/somefile-that-is-only-in-mypackage" ]; then
+if [ ! -f "$srcdir/src/revelation.in" ]; then
  echo "$srcdir doesn't look like source directory for $PKG_NAME" >&2
  exit 1
 fi
